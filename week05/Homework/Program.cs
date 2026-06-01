4f28494020bf8b4facbd203f1c95627df5a2addb
wenndeymae <wenndeymae14@gmail.com>
2026-06-01 11:07:45 +0800

    Inheritance

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        // Test base class
+        Assignment a1 = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(a1.GetSummary());
+        Console.WriteLine();
+
+        // Test MathAssignment
+        MathAssignment math = new MathAssignment(
+            "Roberto Rodriguez",
+            "Fractions",
+            "7.3",
+            "8-19"
+        );
+
+        Console.WriteLine(math.GetSummary());
+        Console.WriteLine(math.GetHomeworkList());
+        Console.WriteLine();
+
+        // Test WritingAssignment
+        WritingAssignment writing = new WritingAssignment(
+            "Mary Waters",
+            "European History",
+            "The Causes of World War II"
+        );
+
+        Console.WriteLine(writing.GetSummary());
+        Console.WriteLine(writing.GetWritingInformation());
     }
 }
