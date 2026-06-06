fc96cf2407b644fc6e12855641faa42f1d8a4d1b
wenndeymae <wenndeymae14@gmail.com>
2026-06-06 14:25:42 +0800

    Polymorphism

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,21 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Square("Red", 4));
+        shapes.Add(new Rectangle("Blue", 3, 5));
+        shapes.Add(new Circle("Green", 2));
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()}");
+            Console.WriteLine($"Area: {shape.GetArea()}");
+            Console.WriteLine();
+        }
     }
 }
